1a1a55ab3abedb5076c76ae197286277dc039853
Douglas <douglaszaltron@gmail.com>
2018-11-22 11:20:16 -0200

    Fluxo de requisicoes

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 
 const app = express()
 
+const logMiddleware = (req, res, next) => {
+    console.log(`HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`)
+    req.appName = 'GoNode'
+    return next()
+}
+
+app.use(logMiddleware)
+
 app.get('/', (req, res) => {
-    return res.send(`Bem-vindo, ${req.query.name}`)
+    return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`)
 })
 
 app.get('/nome/:name', (req, res) => {
