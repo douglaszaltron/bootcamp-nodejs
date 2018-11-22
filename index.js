de9e5d38d5fac64564d0dea6acc2034a69a194dc
Douglas <douglaszaltron@gmail.com>
2018-11-22 12:55:24 -0200

    Configurando ESLint

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const nunjucks = require('nunjucks')
 const app = express()
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true
+  autoescape: true,
+  express: app,
+  watch: true
 })
 
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({
+  extended: false
+}))
 
 app.set('view engine', 'njk')
 
 const users = ['Mercúrio', 'Vênus', 'Terra', 'Marte', 'Júpiter', 'Saturno', 'Urano', 'Netuno']
 
 app.get('/', (req, res) => {
-    return res.render('list', { users: users })
+  return res.render('list', {
+    users: users
+  })
 })
 
 app.get('/new', (req, res) => {
-    return res.render('new')
+  return res.render('new')
 })
 
 app.post('/create', (req, res) => {
-    users.push(req.body.user);
-    return res.redirect("/")
+  users.push(req.body.user)
+  return res.redirect('/')
 })
 
 app.listen(3000)
