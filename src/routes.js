be7bfaa8dd18a3bfedec112e1d12dc10dac8147d
Douglas <douglaszaltron@gmail.com>
2018-11-23 17:15:03 -0200

    Logout

--- src/routes.js
+++ src/routes.js
@@ -17,7 +17,7 @@ routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.use('/app', authMiddleware)
-
+routes.get('/app/logout', SessionController.destroy)
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
   res.render('dashboard')
