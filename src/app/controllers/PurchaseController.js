f36ebfc50b8ad8407a19295ca2bc81d22a1a3fa8
Douglas <douglaszaltron@gmail.com>
2018-11-28 11:18:29 -0200

    Template de e-mail

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"Douglas Zaltron" <douglaszaltron@gmail.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
