159158064432e159d4b165c34948da1749a7a0c2
Douglas <douglaszaltron@gmail.com>
2018-11-28 12:29:46 -0200

    Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '8b2dca1473b17b',
-    pass: 'e48d6a54ba466c'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
